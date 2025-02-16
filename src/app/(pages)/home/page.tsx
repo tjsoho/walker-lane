296ae69cb227ca4eeb4db47d86c223967b4a9e87
Tjcarroll1 <tjcarroll1@icloud.com>
2025-02-16 17:59:03 +1000

    changed section names

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,12 +1,12 @@
-import { HeroSection } from "./sections/HeroSection";
-import { PromiseSection } from "./sections/PromiseSection";
-import { SupportSection } from "./sections/SupportSection";
-import { WhatWeDo } from "./sections/WhatWeDo";
-import { TargetMarket } from "./sections/TargetMarket";
-import { TestimonialsSection } from "./sections/TestimonialsSection";
-import { QuoteSection } from "./sections/QuoteSection";
-import { AboutMe } from "./sections/AboutMe";
-import { DownloadSection } from "./sections/DownloadSection";
+import { HeroSection } from "./sections/Section1-Hero";
+import { PromiseSection } from "./sections/Section2-Promise";
+import { SupportSection } from "./sections/Section3-Clarity-Confidence-Freedom";
+import { WhatWeDo } from "./sections/Section4-WhatWeDo";
+import { TargetMarket } from "./sections/Section5-WhoWeHelp";
+import { TestimonialsSection } from "./sections/Section6-Testimonials";
+import { QuoteSection } from "./sections/Section7-Quote";
+import { AboutMe } from "./sections/Section8-AboutUs";
+import { DownloadSection } from "./sections/Section9-Download";
 
 export default function HomePage() {
   return (
